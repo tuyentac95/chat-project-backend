32f50d7b0e08451956c900657f944f9791564c66
tuyentac95 <tuyentac95@gmail.com>
2020-11-01 16:01:31 +0700

    tạo endpoint chat

---
+++ src/main/java/com/codegym/controller/AppController.java
@@ -0,0 +1,29 @@
+package com.codegym.controller;
+
+import com.codegym.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+public class AppController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")
+    public void sendMessage(@DestinationVariable String to, Message message) {
+        System.out.println("Handling send message: " + message + " to: " + to);
+        simpMessagingTemplate.convertAndSend("/topic/messages/" + to, message);
+    }
+
+    @GetMapping("/registration/{username}")
+    public ResponseEntity<Void> register(@PathVariable String username) {
+        System.out.println("Register: " + username);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/codegym/model/Message.java
@@ -0,0 +1,15 @@
+package com.codegym.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Message {
+    private String message;
+    private String from;
+}
